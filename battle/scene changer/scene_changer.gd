0faeb3568add1594beca19c316b800a757f271fb
Pouya69 <pooyasalehi69@gmail.com>
2025-01-25 09:53:20 -0800

    Changed transition methods

--- battle/scene changer/scene_changer.gd
+++ battle/scene changer/scene_changer.gd
@@ -1,6 +1,7 @@
 class_name SceneChanger extends CanvasLayer
 
-@export var new_level_path : Resource
+# @export var new_level_path : Resource   # THIS WILL BE PASSED FROM THE FUNCTION CALL
+var new_level_path : String
 var new_level : Node2D
 @export var new_location : Vector2
 
@@ -14,8 +15,15 @@ enum E_change_scene_type {
 func change_scene_animation_event() -> void:
 	match change_scene_type:
 		E_change_scene_type.NEW_LEVEL:
-			get_tree().change_scene_to_file(new_level_path.resource_path)
+			if new_level_path.is_empty() or not new_level_path.is_absolute_path():
+				return
+			get_tree().change_scene_to_file(new_level_path)
 		E_change_scene_type.NEW_LOCATION:
 			pass
 		_:
 			pass
+
+
+func start_transition(in_new_level_path : String) -> void:
+	new_level_path = in_new_level_path
+	$AnimationPlayer.play("scene_change_animation")
