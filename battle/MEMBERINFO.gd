d0a533617b989547c4a56e660bc912b0a985aa8e
3vanJB <163084404+3vanJB@users.noreply.github.com>
2025-01-24 19:42:08 -0800

    Battle1
    AAAAAAAAA

---
+++ battle/MEMBERINFO.gd
@@ -0,0 +1,33 @@
+extends Node
+
+
+
+
+var members = {
+	0:{
+		"name":"Rose",
+		"hp":10, "str":1, "def":1, "mgk":1, "mgkdef":1, "spd":1,
+		"ally":true
+	},
+	1:{
+		"name":"Yoru",
+		"hp":1, "str":1, "def":1, "mgk":1, "mgkdef":1, "spd":1,
+		"ally":true
+	},
+	2:{
+		"name":"Adeline",
+		"hp":1, "str":1, "def":1, "mgk":1, "mgkdef":1, "spd":1,
+		"ally":true
+	},
+}
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ battle/battlemember.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+class_name BattleMember
+
+var stats = {"hp":1, "str":1, "def":1, "mgk":1, "mgkdef":1, "spd":1}
+var maxhp
+var maxshine
+var action
+var membername
+var ID
+var ally
