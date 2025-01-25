a2aa94df8de921d7e560d50d37e246212ba0a387
DuckMask <spamfolder.2345@gmail.com>
2025-01-25 14:57:41 -0800

    options re-update
    I re-updating the options because my version crashed

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -6,8 +6,9 @@ var master = AudioServer.get_bus_index("Master")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var value = db_to_linear(master)
-	value_changed =
+	$"Slider Container/Master Slider".value = db_to_linear(AudioServer.get_bus_volume_db(master))
+	$"Slider Container/Music Slider".value = db_to_linear(AudioServer.get_bus_volume_db(music_bus))
+	$"Slider Container/Sfx Slider".value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus))
 
 
 func _on_exit_pressed() -> void:
@@ -20,19 +21,12 @@ func _on_exit_pressed() -> void:
 
 func _on_music_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(music_bus,linear_to_db(value))
-	if value == -20:
-		AudioServer.set_bus_mute(music_bus,true)
-	else:
-		AudioServer.set_bus_mute(music_bus,false)
-
 
 func _on_sfx_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(sfx_bus,linear_to_db(value))
 
-
 func _on_master_slider_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(master,value)
-	if value == -20:
-		AudioServer.set_bus_mute(master,true)
-	else:
-		AudioServer.set_bus_mute(master,false)
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
