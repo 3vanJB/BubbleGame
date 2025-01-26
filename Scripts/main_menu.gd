6d5cbffa8e105a8a06ec68479bb7b6df656d0357
3vanJB <163084404+3vanJB@users.noreply.github.com>
2025-01-26 10:38:39 -0800

    Fix main menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,26 +1,61 @@
 extends Control
 
+var hoversound = preload("res://SFX/Bubbles SFX Batch 1/UI/SFX_UI_Hover.wav")
+var confirmsound = preload("res://SFX/Bubbles SFX Batch 1/UI/SFX_UI_Confirm.wav")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Audio.switchtotrack(0)
-	Audio.set_pitch(1)
+	$Button_Container/Play.grab_focus()
 
 
 func _on_play_pressed() -> void:
 	# Should transition to level 1
 	# $SceneChanger.start_transition("res://overworld/overworld_level.tscn") 
 	Changer.start_transition("res://overworld/overworld_level.tscn") 
-	$SFX_Click.play()
+	Audio.playeffect(confirmsound)
 
 func _on_options_pressed() -> void:
 	# Transition to Options scene
 	Changer.start_transition("res://option_scene.tscn")
-	$SFX_Click.play()
+	Audio.playeffect(confirmsound)
 
 func _on_credits_pressed() -> void:
 	Changer.start_transition("res://Credits.tscn")
-	$SFX_Click.play()
+	Audio.playeffect(confirmsound)
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 	
+
+
+func _on_play_focus_entered() -> void:
+	Audio.playeffect(hoversound)
+
+
+func _on_play_mouse_entered() -> void:
+	Audio.playeffect(hoversound)
+
+
+func _on_options_focus_entered() -> void:
+	Audio.playeffect(hoversound)
+
+
+func _on_options_mouse_entered() -> void:
+	Audio.playeffect(hoversound)
+
+
+func _on_credits_focus_entered() -> void:
+	Audio.playeffect(hoversound)
+
+
+func _on_credits_mouse_entered() -> void:
+	Audio.playeffect(hoversound)
+
+
+func _on_quit_focus_entered() -> void:
+	Audio.playeffect(hoversound)
+
+
+func _on_quit_mouse_entered() -> void:
+	Audio.playeffect(hoversound)
