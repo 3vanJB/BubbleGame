6c4dfb4268d312fcb7188e17ad91528f861fdbbd
Pouya69 <pooyasalehi69@gmail.com>
2025-01-25 18:29:55 -0800

    fixed bad project

--- overworld/overworld_level.gd
+++ overworld/overworld_level.gd
@@ -10,11 +10,11 @@ var current_character_controlled_index : int = 0
 var teleport_bubbles_in_game : int
 
 func spawn_new_bubble() -> void:
-<<<<<<< HEAD
-	var new_bubble : BubbleTeleport = load("res://battle/teleport bubble/bubble_teleport.tscn").instantiate() as BubbleTeleport
-=======
+
+	# var new_bubble : BubbleTeleport = load("res://battle/teleport bubble/bubble_teleport.tscn").instantiate() as BubbleTeleport
+
 	var new_bubble : BubbleTeleport = load("res://misc/teleport bubble/bubble_teleport.tscn").instantiate() as BubbleTeleport
->>>>>>> origin/pouya
+
 	if new_bubble == null:
 		return
 	teleport_bubbles_in_game += 1
@@ -23,7 +23,7 @@ func _on_bubble_spawn_timer_timeout() -> void:
 	if teleport_bubbles_in_game >= max_bubbles_in_level:
 		return
 	spawn_new_bubble()
-<<<<<<< HEAD
+
 
 
 func transition_to_battle(battle_with_who : Array[EnemyCharacter]) -> void:
@@ -33,5 +33,3 @@ func transition_to_battle(battle_with_who : Array[EnemyCharacter]) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("escape"):
 		$SceneChanger.start_transition("res://Options_UI.tscn")
-=======
->>>>>>> origin/pouya
