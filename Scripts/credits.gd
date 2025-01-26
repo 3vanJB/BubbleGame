10421811790e1fa16bb686401c123a8eb94795c6
DuckMask <spamfolder.2345@gmail.com>
2025-01-26 13:41:01 -0800

    Added a Icon
    No Idea why skill 1 color keeps reverting to black

--- Scripts/credits.gd
+++ Scripts/credits.gd
@@ -1,5 +1,6 @@
 extends Control
-
+var hoversound = preload("res://SFX/Bubbles SFX Batch 1/UI/SFX_UI_Hover.wav")
+var confirmsound = preload("res://SFX/Bubbles SFX Batch 1/UI/SFX_UI_Confirm.wav")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#Audio.switchtotrack(0)
@@ -7,3 +8,8 @@ func _ready() -> void:
 	
 func _on_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main Menu.tscn")
+
+
+
+func _on_exit_mouse_entered() -> void:
+	Audio.playeffect(hoversound)
